46bd8d71d47c0aee1a63a78b52794030d301fda9
oxowl <40059218+oxowl@users.noreply.github.com>
2023-11-30 17:21:04 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,14 +10,13 @@ type Config = {
 const testnetConfig: Config = {
     rpcUrl: 'https://1rpc.io/base-goerli',
     chainId: 84531,
-    address: '0xB9a02142b36eA3e1b612522535F36957525C62Ee'
+    address: '0x40c453c3d8164D458869Dbe2cbf430c6B1e6e1b9'
 }
 
 const mainnetConfig: Config = {
-    // todo
-    rpcUrl: '',
+    rpcUrl: 'https://mainnet.base.org', //Rate limited and not for production systems.
     chainId: 8453,
-    address: ''
+    address: '0x40c453c3d8164D458869Dbe2cbf430c6B1e6e1b9'
 }
 
 export const getConfig = () => {
